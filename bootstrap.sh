bbc4926467fa37485d0e1b4632ad0d2b8283b38f
Patrick Hogan <phogan@uber.com>
2017-01-05 10:11:36 -0800

    changed default arc editor and removed dock/dashboard configs

--- bootstrap.sh
+++ bootstrap.sh
@@ -53,7 +53,7 @@ brew cask cleanup
 # Arcanist                                                                    #
 ###############################################################################
 
-arc set-config editor "/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
+arc set-config editor "/usr/local/bin/vim"
 
 ###############################################################################
 # iTerm2                                                                      #
@@ -161,27 +161,11 @@ defaults write com.apple.dock magnification -bool true
 defaults write com.apple.dock tilesize -int 15
 defaults write com.apple.dock largesize -int 111
 
-# Set the icon size of Dock items to 36 pixels
-defaults write com.apple.dock tilesize -int 36
-
-# Change minimize/maximize window effect
-defaults write com.apple.dock mineffect -string "scale"
-
-# Minimize windows into their application’s icon
-defaults write com.apple.dock minimize-to-application -bool true
-
-# Show indicator lights for open applications in the Dock
-defaults write com.apple.dock show-process-indicators -bool true
+# Set the icon size of Dock items
+defaults write com.apple.dock tilesize -int 25
 
 # Speed up Mission Control animations
 defaults write com.apple.dock expose-animation-duration -float 0.1
-
-# Don’t group windows by application in Mission Control
-# (i.e. use the old Exposé behavior instead)
-defaults write com.apple.dock expose-group-by-app -bool false
-
-# Don’t show Dashboard as a Space
-defaults write com.apple.dock dashboard-in-overlay -bool true
 
 # Don’t automatically rearrange Spaces based on most recent use
 defaults write com.apple.dock mru-spaces -bool false
@@ -189,20 +173,11 @@ defaults write com.apple.dock mru-spaces -bool false
 # Remove the auto-hiding Dock delay
 defaults write com.apple.dock autohide-delay -float 0
 
-# Remove the animation when hiding/showing the Dock
-defaults write com.apple.dock autohide-time-modifier -float 0
-
 # Automatically hide and show the Dock
 defaults write com.apple.dock autohide -bool true
 
-# Make Dock icons of hidden applications translucent
-defaults write com.apple.dock showhidden -bool true
-
 # Disable the Launchpad gesture (pinch with thumb and three fingers)
 defaults write com.apple.dock showLaunchpadGestureEnabled -int 0
-
-# Disable Dashboard
-defaults write com.apple.dashboard mcx-disabled -bool true
 
 # Apps
 defaults delete com.apple.dock persistent-apps
